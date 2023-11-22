44518c746c3c58f14c92524ac63e89446b35ff05
sagor66 <sazzadulhaquesagor@gmail.com>
2023-11-23 01:29:48 +0600

    done create student feature

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response, Application } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
